a4cb7a5a72ff9a0cd98986cd4172ade85ac33861
Iaannn06 <alfianazad05@gmail.com>
2024-05-20 18:42:07 +0700

    membuat inheritance

---
+++ Inheritance/Inheritance.cpp
@@ -0,0 +1,43 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+class orang {
+public:
+	string nama;
+
+	orang(string pNama) :
+		nama(pNama) {
+		cout << "orang dibuat\n" << endl;
+	}
+	~orang() {
+		cout << "orang dihapus\n" << endl;
+	}
+	int jumlah(int a, int b) {
+		return a + b;
+	}
+};
+
+class pelajar :public orang {
+public:
+	string sekolah;
+
+	pelajar(string pNama, string pSekolah) : orang(pNama), sekolah(pSekolah) {
+		cout << "Pelajar dibuat\n" << endl;
+	}
+	~pelajar() {
+		cout << "Pelajar dihapus\n" << endl;
+	}
+	string perkenalan() {
+		return "Hallo, nama saya " + nama + "Dari sekolah " + sekolah + "\n\n";
+	}
+};
+
+int main() {
+	pelajar siswa1("Andi Laksono", "SMAN 1 Bantul");
+	cout << siswa1.perkenalan();
+	cout << "Hasil = " << siswa1.jumlah(10, 90) << endl;
+
+	return 0;
+}
+
--- MainIA/MainIA.cpp
+++ MainIA/MainIA.cpp
@@ -6,9 +6,24 @@ using namespace std;
 
 int main() {
 	ibu* varIbu = new ibu("dini");
-	ibu* varIbu2 = new ibu("Novi";
+	ibu* varIbu2 = new ibu("Novi");
 	anak* varAnak1 = new anak("tono");
 	anak* varAnak2 = new anak("Rini");
 	anak* varAnak3 = new anak("Dewi");
 
+	varIbu->tambahAnak(varAnak1);
+	varIbu->tambahAnak(varAnak2);
+	varIbu2->tambahAnak(varAnak3);
+	varIbu2->tambahAnak(varAnak1);
+
+	varIbu->cetakAnak();
+	varIbu->cetakAnak();
+
+	delete varIbu;
+	delete varIbu2;
+	delete varAnak1;
+	delete varAnak2;
+	delete varAnak3;
+
+	return 0;
 }
